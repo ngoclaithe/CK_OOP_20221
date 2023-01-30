605282beeeea860db0f7cec0aee533e503a89f8f
Lai The Ngoc <laingoc99@gmail.com>
2023-01-30 19:31:17 +0700

    update 1

---
+++ CKOOP/src/E.java
@@ -0,0 +1,22 @@
+
+public class E {
+	String title;
+	String descrip;
+	public String getTitle()
+	{
+		return title;		
+	}
+	public void setTitle(String title)
+	{
+		this.title=title;
+	}
+	public String getDes()
+	{
+		return descrip;		
+	}
+	public void setDes(String descrip)
+	{
+		this.descrip=descrip;
+	}
+
+}
